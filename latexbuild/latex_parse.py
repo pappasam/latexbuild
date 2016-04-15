2e1516ee89fb0af69733bab259cbb38a3f8a614c
Sam Roeca <samuel.roeca@gmail.com>
2016-04-15 09:47:00 -0400

    Update latex parser to better-handle edge cases

--- latexbuild/latex_parse.py
+++ latexbuild/latex_parse.py
@@ -8,13 +8,34 @@ import re
 ######################################################################
 # Latex escape regex constants
 ######################################################################
+
+# Organize all latex escape characters in one list
+# (EXCEPT FOR ( "\" ), which is handled separately)
+# escaping those which are special characters in
+# PERL regular expressions
 ESCAPE_CHARS = [r'\&', '%', r'\$', '#', '_', r'\{', r'\}', '~', r'\^', ]
-ESCAPE_CHARS_OR = '[{}]'.format('|'.join(ESCAPE_CHARS))
+
+# For each latex escape character, create a regular expression
+# that matches all of the following criteria
+# 1) two characters
+# 2) the first character is NOT a backslash ( "\" )
+# 3) the second character is one of the latex escape characters
 REGEX_ESCAPE_CHARS = [
         (re.compile(r"(?=[^\\])" + i), r"\\" + i.replace('\\', ''))
         for i in ESCAPE_CHARS
         ]
-REGEX_BACKSLASH = re.compile(r'\\(?!{})'.format(ESCAPE_CHARS_OR))
+
+# Place escape characters in [] for "match any character" regex
+ESCAPE_CHARS_OR = r'[{}\\]'.format(ESCAPE_CHARS)
+
+# For the back slash, create a regular expression
+# that matches all of the following criteria
+# 1) three characters
+# 2) the first character is not a backslash
+# 3) the second characters is a backslash
+# 4) the third character is none of the ESCAPE_CHARS,
+#       and is also not a backslash
+REGEX_BACKSLASH = re.compile(r'(?!\\)\\(?!{})'.format(ESCAPE_CHARS_OR))
 
 ######################################################################
 # Declare module functions
